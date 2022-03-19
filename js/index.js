90b243c90b0435bb5248f56a92848eb0ce0af8e6
Júnior Dering <juniordering@hotmail.com>
2022-03-19 11:23:51 -0400

    enviando arquivos do exercicio de Dom da Gama

---
+++ js/index.js
@@ -0,0 +1,31 @@
+// let header = document.querySelector('header')
+// header.style.backgroundColor = '#2e948a'
+
+// document.querySelector('#introducao').style.justifyContent = 'space-around'
+// document.querySelectorAll('#menu_opcoes nav a')[0].setAttribute('href','/cursos.html')
+// // document.querySelectorAll('#menu_opcoes nav a')[0].href = '/cursos.html'
+// document.querySelector('.depoimento h3').innerText = 'O que falam sobre nos'
+// document.querySelectorAll('.titulo h3')[1].innerText = 'Mais conteúdo pra você.'
+// // document.querySelectorAll('.titulo h3').forEach(item => item.toUpperCase())
+// let titulo = document.querySelectorAll('.titulo h3')
+// titulo.forEach(item=>item.innerHTML = item.innerHTML.toUpperCase())
+// document.querySelector('#todos_posts').href = '/blog.html'
+
+// let div = document.createElement('div')
+// div.innerHTML = 
+//     '<div id="independencia">'+
+//         '<img src="/imagens/independencia_financeira.png" width="180px" alt="Independência Financeira"/>' +
+//         '<h2>Independência Financeira</h2>' +
+// '        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat      nulla     ariatur.</p>' +
+//         '<a class="comecar_agora" href="./curso.html">começar agora</a>' +
+//     '</div>'
+
+// let divprimeiro = document.querySelector('#investimentos')
+
+// divprimeiro.insertAdjacentElement("afterend",div)
+
+document.querySelector('form').action = '/sucesso.html'
+
+document.querySelectorAll('#formulario form input')
+console.log(document.querySelectorAll('#formulario form input')[1]);
+
